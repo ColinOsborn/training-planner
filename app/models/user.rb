eb0be2bc3b8b1f8e7232b54990d12eb9c5741f3d
Colin Osborn <colin.osborn@gmail.com>
2016-06-15 11:07:45 -0600

    refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
 
   enum role: ["coach", "admin", "athlete"]
-  #
-  # def password
-  #   @password ||= Password.new(password_digest)
-  # end
+  
+  def password
+    @password ||= Password.new(password_digest)
+  end
 
   def password=(new_password)
     Password.create(new_password)
